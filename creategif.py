0e306de018ad30b6eeac92e9fb9ad3e229a27381
Yann Thorimbert <yann.thorimbert@gmail.com>
2019-06-25 12:46:50 +0200

    Update creategif.py

--- creategif.py
+++ creategif.py
@@ -11,16 +11,18 @@ end = 230
 step = 1
 duration = 75
 loop = 0
+dst = "result.gif"
 
 
 i = 0
 frames = []
 for fn in os.listdir("./"):
-    if patter in fn:
+    if pattern in fn:
         if start <= i <= end:
             if i%step == 0:
                 print(i, fn)
                 frames.append(Image.open(fn))
             i += 1
 
-frames[0].save('result.gif', format='GIF', append_images=frames[1:], save_all=True, duration=duration, loop=loop)
+frames[0].save(dst, format='GIF', append_images=frames[1:],
+                save_all=True, duration=duration, loop=loop)

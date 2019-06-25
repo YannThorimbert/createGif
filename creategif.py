b76f97291281c2f0367319991198ee18a7ab11db
YannThorimbert <yann.thorimbert@gmail.com>
2019-06-25 12:41:55 +0200

    added main file

---
+++ creategif.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import os, sys
+
+if len(sys.argv) > 1:
+    pattern = sys.argv[1]
+else:
+    pattern = ".png"
+
+start = 0
+end = 230
+step = 1
+duration = 75
+loop = 0
+
+
+i = 0
+frames = []
+for fn in os.listdir("./"):
+    if patter in fn:
+        if start <= i <= end:
+            if i%step == 0:
+                print(i, fn)
+                frames.append(Image.open(fn))
+            i += 1
+
+frames[0].save('result.gif', format='GIF', append_images=frames[1:], save_all=True, duration=duration, loop=loop)
